e83bed40ac26ffa2a3d462e87d2dff97003478c0
carolaine-viana <carolsantos14@hotmail.com>
2021-03-01 15:02:35 -0300

    login-atual

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import style from "./Style.css";
-// import logo from "../../Assets/img/logo.png";
+// import logo from "../../Assets/logo.png";
 import TextField from "@material-ui/core/TextField";
-import {goToSignUp} from '../../Routes/Coordinator';
+import {goToSignUp} from '../../components/Route/Coordinator';
 import { useHistory } from "react-router-dom";
 
 const Login = () => {
--- src/components/Pages/SignUp/SignUp.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import style from "./Style.css";
-
-const SignUp = () => {
-  return <h1>teste</h1>;
-}
-
-export default SignUp;
