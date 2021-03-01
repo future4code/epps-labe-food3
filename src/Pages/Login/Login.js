e2fc238ac4540a794b1a0ccc81c8c83068de3103
carolaine-viana <carolsantos14@hotmail.com>
2021-03-01 14:54:49 -0300

    login-atual

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from "react";
 import style from "./Style.css";
-import logo from '../../Assets/img/logo-future-eats.png';
+// import logo from "../../Assets/img/logo.png";
+import TextField from "@material-ui/core/TextField";
+import {goToSignUp} from '../../Routes/Coordinator';
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+    const history = useHistory();
+
   return (
-    <div>
-        <img src={logo} className="logo"/>
-      <h1>Login Page</h1>
+    <div className="main">
+        {/* <img src={logo} /> */}
+
+      <h1 className="Text">Entrar</h1>
+      <TextField
+        id="outlined-basic"
+        label="email@email.com"
+        variant="outlined"
+        placeholder="email@email.com"
+        className="Input_nolocus"
+      />
+      <TextField
+        id="outlined-basic"
+        label="Minimo 6 caracteres"
+        variant="outlined"
+        className="Input_nolocus"
+      />
+      <button className="Rectangle">Entrar</button>
+      <p>Nao possui cadastro? <u>clique aqui</u></p>
+      <button onClick={()=>goToSignUp(history)}>Cadastrar</button>
     </div>
   );
 };
