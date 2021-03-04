09b1cb1fd6e252469949f69b331f7fad0c0d1efc
Jose Modolo <josenetozxc@yahoo.com>
2021-03-04 20:23:41 -0300

    loading page done

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -4,25 +4,58 @@ import { useHistory } from "react-router-dom";
 import style from './Style.css';
 import logo from "../../Assets/logo-future-eats@3x.png";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import styled from 'styled-components'
+
+
+const LogoFutureEats = styled.img`
+  width: 126px;
+  height: 65px;
+  
+`
+
+const LoadingContainer = styled.div`
+  /* width: 100%; */
+`
+
+const CentralDiv = styled.div`
+background-color: black;
+width: 600px;
+height: 600px;
+display: flex;
+align-items: center;
+justify-content: center;
+
+
+`
+
+const MainDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+`
 
 const Loading = () => {
     const history = useHistory();
     
-    useEffect(() => {
-        const timer = setTimeout(() => {
-          // console.log('This will run after 5 second!')
-          goToLogin(history)
-        }, 5000);
-        return () => clearTimeout(timer);
-      }, []);
+//  useEffect(() => {
+//      const timer = setTimeout(() => {          
+//        goToLogin(history)
+//      }, 5000);
+//      return () => clearTimeout(timer);
+//    }, []);
       
       return(
-        <div>
-            <img src={logo} className="Logo_FutureEats"/>
-            <div className="LoadingContainer">
+        <MainDiv>
+          <CentralDiv>
+            <LogoFutureEats src={logo}/>
+            <LoadingContainer>
               <CircularProgress color="secondary" />
-            </div>
-        </div>
+            </LoadingContainer>
+          </CentralDiv>
+        </MainDiv>
+        
       )
 }
 
